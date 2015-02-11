7c062b9a23638211b7ccd32d0ec4a10ac09f75d5
yuansl <yuansl2111@126.com>
2015-02-11 21:36:56 +0800

    towSum done

---
+++ two_sum.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> twoSum(vector<int> &numbers, int target)
+{
+	int i, j, n;
+	vector<int> ret;
+	n = numbers.size();
+
+	for (i = 0; i < n - 1; i++)
+		for (j = i + 1; j < n; j++)
+			if (numbers[i] + numbers[j] == target) {
+				ret.push_back(i);
+				ret.push_back(j);
+				goto succ_exit;
+			}
+succ_exit:
+	return ret;
+}
