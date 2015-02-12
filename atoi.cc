57f58d00649ef0f9af05e5a04ecbf51c4884e0b3
yuansl <yuansl2111@126.com>
2015-02-12 21:10:51 +0800

    String to Integer

---
+++ atoi.cc
@@ -0,0 +1,32 @@
+#include <climits>
+#include <string>
+using namespace std;
+
+int atoi_r(string str)
+{
+	int ret, i, sign;
+	
+	ret = 0;
+	i = 0;
+	sign = 1;
+	while (str[i] && isspace(str[i]))
+		i++;
+
+	if (str[i] && !isdigit(str[i])) {
+		if (str[i] == '+') {
+			sign = 1;
+			i++;
+		} else if (str[i] == '-') {
+			sign = -1;
+			i++;
+		} else
+			return 0;
+	}
+	while (str[i] && isdigit(str[i])) {
+		ret = 10 * ret + str[i] - '0';
+		i++;
+	}
+	if (ret + INT_MAX == -1 && sign != -1)
+		ret = INT_MAX;
+	return ret * sign;
+}
